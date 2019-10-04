84a3d2c994a8640a9bae9452ea4e0e97660b978d
Mr.Lutin <mcotemw@icloud.com>
2019-10-04 19:44:29 -0400

    Update start_server.sh
    Change wget to wget --no-cache

--- start_server.sh
+++ start_server.sh
@@ -11,10 +11,10 @@ else
     BRANCH="$1"
 fi
 
-wget "https://cdn.altv.mp/server/$BRANCH/x64_linux/altv-server" -O ./altv-server
-wget "https://cdn.altv.mp/node-module/$BRANCH/x64_linux/libnode.so.72" -O ./libnode.so.72
-wget "https://cdn.altv.mp/node-module/$BRANCH/x64_linux/modules/libnode-module.so" -O ./modules/libnode-module.so
-wget "https://cdn.altv.mp/server/$BRANCH/x64_linux/data/vehmodels.bin" -O ./data/vehmodels.bin
-wget "https://cdn.altv.mp/server/$BRANCH/x64_linux/data/vehmods.bin" -O ./data/vehmods.bin
+wget --no-cache "https://cdn.altv.mp/server/$BRANCH/x64_linux/altv-server" -O ./altv-server
+wget --no-cache "https://cdn.altv.mp/node-module/$BRANCH/x64_linux/libnode.so.72" -O ./libnode.so.72
+wget --no-cache "https://cdn.altv.mp/node-module/$BRANCH/x64_linux/modules/libnode-module.so" -O ./modules/libnode-module.so
+wget --no-cache "https://cdn.altv.mp/server/$BRANCH/x64_linux/data/vehmodels.bin" -O ./data/vehmodels.bin
+wget --no-cache "https://cdn.altv.mp/server/$BRANCH/x64_linux/data/vehmods.bin" -O ./data/vehmods.bin
 
 ./altv-server --config "config/server.cfg" --logfile "logs/server.log
