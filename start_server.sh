25332590a875fb13c23ccb5727351c98a79c19c8
Mr.Lutin <mcotemw@icloud.com>
2019-10-04 19:40:48 -0400

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -2,4 +2,19 @@
 BASEDIR=$(dirname $0)
 export LD_LIBRARY_PATH=${BASEDIR}
 cd ${BASEDIR}
-./altv-server --config "config/server.cfg" --logfile "logs/server.log"
+
+if [ -z "$1" ]
+
+then
+    BRANCH=beta
+else
+    BRANCH="$1"
+fi
+
+wget "https://cdn.altv.mp/server/$BRANCH/x64_linux/altv-server" -O ./altv-server
+wget "https://cdn.altv.mp/node-module/$BRANCH/x64_linux/libnode.so.72" -O ./libnode.so.72
+wget "https://cdn.altv.mp/node-module/$BRANCH/x64_linux/modules/libnode-module.so" -O ./modules/libnode-module.so
+wget "https://cdn.altv.mp/server/$BRANCH/x64_linux/data/vehmodels.bin" -O ./data/vehmodels.bin
+wget "https://cdn.altv.mp/server/$BRANCH/x64_linux/data/vehmods.bin" -O ./data/vehmods.bin
+
+./altv-server --config "config/server.cfg" --logfile "logs/server.log
