9bf1553855214a939e6137b4bf2776ec77958dc0
Mr.Lutin <mcotemw@icloud.com>
2019-10-04 19:58:16 -0400

    Update start_server.sh
    change brand for Beta

--- start_server.sh
+++ start_server.sh
@@ -3,18 +3,10 @@ BASEDIR=$(dirname $0)
 export LD_LIBRARY_PATH=${BASEDIR}
 cd ${BASEDIR}
 
-if [ -z "$1" ]
-
-then
-    BRANCH=beta
-else
-    BRANCH="$1"
-fi
-
-wget --no-cache "https://cdn.altv.mp/server/$BRANCH/x64_linux/altv-server" -O ./altv-server
-wget --no-cache "https://cdn.altv.mp/node-module/$BRANCH/x64_linux/libnode.so.72" -O ./libnode.so.72
-wget --no-cache "https://cdn.altv.mp/node-module/$BRANCH/x64_linux/modules/libnode-module.so" -O ./modules/libnode-module.so
-wget --no-cache "https://cdn.altv.mp/server/$BRANCH/x64_linux/data/vehmodels.bin" -O ./data/vehmodels.bin
-wget --no-cache "https://cdn.altv.mp/server/$BRANCH/x64_linux/data/vehmods.bin" -O ./data/vehmods.bin
+wget --no-cache "https://cdn.altv.mp/server/beta/x64_linux/altv-server" -O ./altv-server
+wget --no-cache "https://cdn.altv.mp/node-module/beta/x64_linux/libnode.so.72" -O ./libnode.so.72
+wget --no-cache "https://cdn.altv.mp/node-module/beta/x64_linux/modules/libnode-module.so" -O ./modules/libnode-module.so
+wget --no-cache "https://cdn.altv.mp/server/beta/x64_linux/data/vehmodels.bin" -O ./data/vehmodels.bin
+wget --no-cache "https://cdn.altv.mp/server/beta/x64_linux/data/vehmods.bin" -O ./data/vehmods.bin
 
 ./altv-server --config "config/server.cfg" --logfile "logs/server.log
