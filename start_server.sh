d615f756d9d2aeef0f772d6c8875043d17e1461b
Mr.Lutin <mcotemw@icloud.com>
2021-04-08 15:09:29 -0400

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -7,7 +7,7 @@ wget --no-cache "https://cdn.altv.mp/server/release/x64_linux/altv-server" -O ./
 
 mkdir ./modules/js-module
 wget --no-cache "https://cdn.altv.mp/node-module/release/x64_linux/libnode.so.72" -O ./modules/js-module/libnode.so.72
-wget --no-cache "https://cdn.altv.mp/js-module/release/x64_linux/modules/js-module/libjs-module.so " -O ./modules/js-module/libjs-module.so
+wget --no-cache "https://cdn.altv.mp/js-module/release/x64_linux/modules/js-module/libjs-module.so" -O ./modules/js-module/libjs-module.so
 
 wget --no-cache "https://cdn.altv.mp/server/release/x64_linux/data/vehmodels.bin" -O ./data/vehmodels.bin
 wget --no-cache "https://cdn.altv.mp/server/release/x64_linux/data/vehmods.bin" -O ./data/vehmods.bin
