463dffa7f5f0f02c819031b9ff2a01e2cd9a4a40
Mr.Lutin <mcotemw@icloud.com>
2021-04-08 14:54:16 -0400

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -4,8 +4,8 @@ export LD_LIBRARY_PATH=${BASEDIR}
 cd ${BASEDIR}
 
 wget --no-cache "https://cdn.altv.mp/server/release/x64_linux/altv-server" -O ./altv-server
-wget --no-cache "https://cdn.altv.mp/node-module/release/x64_linux/libnode.so.72" -O ./libnode.so.72
-wget --no-cache "https://cdn.altv.mp/node-module/release/x64_linux/modules/libnode-module.so" -O ./modules/libnode-module.so
+wget --no-cache "https://cdn.altv.mp/node-module/release/x64_linux/libnode.so.72" -O ./modules/js-module/libnode.so.72
+wget --no-cache "https://cdn.altv.mp/js-module/release/x64_linux/modules/js-module/libjs-module.so " -O ./modules/js-module/libjs-module.so
 wget --no-cache "https://cdn.altv.mp/server/release/x64_linux/data/vehmodels.bin" -O ./data/vehmodels.bin
 wget --no-cache "https://cdn.altv.mp/server/release/x64_linux/data/vehmods.bin" -O ./data/vehmods.bin
 
